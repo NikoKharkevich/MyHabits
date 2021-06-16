215233a48649413796aef574b430216895d98845
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-06-16 18:02:33 +0300

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Nikola Kharkevich on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
